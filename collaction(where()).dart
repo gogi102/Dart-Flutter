db78c62bc1f35e6804922c1d7f5cf26f5e015ddd
Yang jun seog <127902276+gogi102@users.noreply.github.com>
2024-05-03 07:33:14 +0900

    Update collaction(where()).dart

--- collaction(where()).dart
+++ collaction(where()).dart
@@ -1,11 +1,5 @@
 void main() {
   List<String> food = ['햄버거', '치킨', '피자'];
-  print(food);
-  print(food[0]);
-  print(food[2]);
-  print(food.length);
-  food[2] = '짜장면'; // food 리스트의 2번째 값을 짜장면으로 바꾼다
-  print(food);
 
   food.add('콜라'); //add() 함수는 리스트의 맨 마지막 값에 value값을 추가하는 함수이다.
   print(food);
