2503b63cd9f9b01a13446fe891c1f60712ed0e23
Yang jun seog <127902276+gogi102@users.noreply.github.com>
2024-05-13 12:30:15 +0900

    Delete final,const.dart

--- final,const.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  final String name1 = '양준석';
-  const String name2 = '장연준';
-
-  final DateTime now = DateTime.now();
-  print(now);//DateTime 함수 같은 경우에는 코드가 실행되는 순간을 
-					   //기준으로 시간을 구해주는 함수이기 때문에 
-					   //const가 아닌 final로 써줘야한다.
-}
