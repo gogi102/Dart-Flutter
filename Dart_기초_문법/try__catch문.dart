17a3e32485a223cbd774860e13d4ce5916e1e80d
Yang jun seog <127902276+gogi102@users.noreply.github.com>
2024-05-16 10:09:56 +0900

    Update try__catch문.dart

--- Dart_기초_문법/try__catch문.dart
+++ Dart_기초_문법/try__catch문.dart
@@ -2,8 +2,8 @@ void main() {
   try {
     final String name = '양준석';
 
-    throw Exception('이름이 잘못됐습니다');
-
+    throw Exception('이름이 잘못됐습니다'); //throw키워드로 고의적을 오류 발생
+    //오류가 없을경우 그냥 이름 출력
     print(name);
   } catch (e) {
     print(e);
