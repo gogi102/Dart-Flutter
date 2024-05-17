f8ca06bbdbb7c616e246dbec5088a57569c47612
Yang jun seog <127902276+gogi102@users.noreply.github.com>
2024-05-17 12:06:32 +0900

    Update enum.dart

---
+++ Dart_기초_문법/enum.dart
@@ -0,0 +1,12 @@
+enum Status {
+  approved,
+  pending,
+  rejected,
+}
+
+void main() {
+  Status status = Status.approved;
+  print(status);
+}
+// enume은 한 변수의 값을 몇 가지 옵션으로 제한하는 기능이다.
+// 선택지가 제한적일 때 사용한다.
