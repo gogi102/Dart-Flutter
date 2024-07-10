b5b8a4302122e228782746830ca5663051ce78ab
Yang jun seog <127902276+gogi102@users.noreply.github.com>
2024-07-10 14:07:29 +0900

    Add files via upload

---
+++ SSS_APP_Study/bottomNavigationBar/homePage.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class homePage extends StatelessWidget {
+  const homePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.2,
+                  ),
+                  const Text(
+                    textAlign: TextAlign.center,
+                    "홈 페이지",
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 38,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ SSS_APP_Study/bottomNavigationBar/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/homePage.dart';
+import 'package:flutter_application_1/informations.dart';
+import 'package:flutter_application_1/to_do.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Miso(),
+    );
+  }
+}
+
+class Miso extends StatefulWidget {
+  const Miso({super.key});
+
+  @override
+  State<Miso> createState() => _MisoState();
+}
+
+class _MisoState extends State<Miso> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        // 메뉴를 이동시 초기화를 막아줌?
+        index: currentIndex,
+        children: const [
+          homePage(),
+          todoPage(),
+          informationPage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex, // 현재 보여주는 탭
+        onTap: (newIndex) {
+          print("selected newIndex : $newIndex");
+          // 다른 페이지로 이동
+          setState(() {
+            currentIndex = newIndex;
+          });
+        },
+        selectedItemColor: Colors.blueAccent, // 선택됬을때
+        unselectedItemColor: Colors.grey, // 선택되지 않았을때
+        backgroundColor: Colors.white,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "홈",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: "Todo",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "내정보",
+          ),
+        ],
+      ),
+    );
+  }
+}
